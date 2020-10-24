81dd443668f6a2f68296e6f05c8fb06e289632ca
L.AQ <50367996+anqi9@users.noreply.github.com>
2020-10-24 20:59:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 import time
 from GB import GB
+from MasterProblem import MasterProblem
 
 def main():
     data = create_data_model()
+    '''using DW-decomposition to solve the routing & scheduling problem'''
+    opt = MasterProblem(data)
+    [cost, routes] = opt.Mater_Problem()
+   
+    ''' to solve the routing&scheduling problem directly '''
     # to create an optimization model
     opt_GB = GB(data)
     # to solve the model using Gurobi solver
